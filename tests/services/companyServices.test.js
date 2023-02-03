0901fc00778eab22dcf8d186f48a64172db5734e
Bharath Raj <BharathRajgngowdara@gmail.com>
2023-02-03 15:54:38 +0530

    feat: add tests for services

--- tests/services/companyServices.test.js
+++ tests/services/companyServices.test.js
@@ -33,3 +33,80 @@ describe('get all scores', () => {
       .toEqual(score);
   });
 });
+
+describe('get all companies by sector', () => {
+
+  const companies = [{
+    'id': '46e1d061-e39d-4d5c-8e0e-3fa5d45d9efc',
+    'cpi': 0.32,
+    'cf': 899309,
+    'mau': 0.28,
+    'roic': 24.02,
+    'name': 'Apple',
+    'ceo': 'Ken Trantow',
+
+  }, {
+    'id': 'b6472c52-732a-4fd2-a463-ae604c0a2c79',
+    'cpi': 0.98,
+    'cf': 513484,
+    'mau': 0.55,
+    'roic': 18.64,
+    'name': 'Microsoft',
+    'ceo': 'Abel Koch',
+  }];
+  const score = [
+    {
+      'id': '46e1d061-e39d-4d5c-8e0e-3fa5d45d9efc',
+      'name': 'Apple',
+      'ceo': 'Ken Trantow',
+      'score': 29.987724999999998,
+      'ranking': 1
+    },
+    {
+      'id': 'b6472c52-732a-4fd2-a463-ae604c0a2c79',
+      'name': 'Microsoft',
+      'ceo': 'Abel Koch',
+      'score': 21.3221,
+      'ranking': 2
+    },
+  ];
+
+  it('should return list of all companies by sector & correct ranking', async () => {
+    const spiedFindAll = jest.spyOn(Company, 'findAll')
+      .mockResolvedValue(companies);
+
+    const returedVal = await companyServices.getCompaniesBySector();
+    expect(spiedFindAll).toBeCalled();
+    expect(returedVal)
+      .toEqual(score);
+  });
+});
+
+describe('update company name or ceo of company', () => {
+  const sector = 'Automobile';
+  const score = [
+    {
+      'id': '95b5a067-808a-44a9-a490-b4ef8a045f61',
+      'name': 'NAME',
+      'ceo': 'CEO',
+      'sector_name': sector
+    }
+  ];
+  const companies = [
+    {
+      'id': '95b5a067-808a-44a9-a490-b4ef8a045f61',
+      'name': 'Volkswagen',
+      'ceo': 'April Padberg'
+    }
+  ];
+
+  it('should return list of all companies with scores', async () => {
+    const spiedFindAll = jest.spyOn(Company, 'findByPk')
+      .mockResolvedValue(companies);
+
+    // const returedVal = await companyServices.updateCompanyData(sector, 'CEO', 'NAME');
+    // expect(spiedFindAll).toBeCalled();
+    // expect(returedVal)
+    //   .toEqual(score);
+  });
+});
