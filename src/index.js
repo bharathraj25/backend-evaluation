a36dfbdad04e9f4ee654d148d67b57afc242e547
Bharath Raj <BharathRajgngowdara@gmail.com>
2023-02-03 11:44:21 +0530

    feat: set up server and add basic files

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const { PORT } = require('./config');
+
+const app = express();
+
+app.use(express.json());
+
+const apiRoutes = require('./routes');
+app.use('/api', apiRoutes);
+
+app.listen(PORT, () => {
+  console.log('server started visit http://localhost:', PORT);
+});
